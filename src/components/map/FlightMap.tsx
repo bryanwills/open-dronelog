46f43d8434eab7af95f37ffd8fbe23dd761747ed
Arpan Ghosh <26424944+arpanghosh8453@users.noreply.github.com>
2026-03-05 14:44:53 +0100

    feat: Set the map's minimum zoom level to 10

--- src/components/map/FlightMap.tsx
+++ src/components/map/FlightMap.tsx
@@ -1433,6 +1433,7 @@ export function FlightMap({ track, homeLat, homeLon, durationSecs, telemetry, th
     >
       <Map
         {...viewState}
+        minZoom={10}
         maxZoom={22}
         maxPitch={is3D ? 85 : 0}
         style={{ width: '100%', height: '100%', position: 'absolute', top: '0', right: '0', bottom: '0', left: '0' }}
